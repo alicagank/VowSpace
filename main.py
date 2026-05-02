41d7e427647e2010af38fa5b17c869842be459d2
Ali Çağan Kaya <alicagank@icloud.com>
2026-05-03 01:50:11 +0300

    Absolute path to resource for app icon

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ A büg once bit my sister... No realli!
 Mynd you, büg bites Kan be pretti nasti...
 """
 import sys
+from pathlib import Path
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
@@ -13,11 +14,22 @@ from PyQt5.QtGui import QIcon
 from vowspace.vowel_space_visualizer import VowelSpaceVisualizer
 
 
+def resource_path(relative_path):
+    # Get absolute path to resource
+    if hasattr(sys, "_MEIPASS"):
+        return Path(sys._MEIPASS) / relative_path
+    return Path(relative_path)
+
+
 def main():
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon("vowspace/assets/vowspace-1024.png"))
+
+    icon_path = resource_path("vowspace/assets/vowspace-1024.png")
+    app.setWindowIcon(QIcon(str(icon_path)))
+
     window = VowelSpaceVisualizer()
     window.show()
+
     sys.exit(app.exec_())
 
 
